834fa8194c76710c1ec695ad6df2b3d4a87edf52
r_takaishi <ryo.takaishi.0@gmail.com>
2018-05-18 09:22:32 +0900

    update

--- lib/fluent/plugin/in_proxysql_query_log.rb
+++ lib/fluent/plugin/in_proxysql_query_log.rb
@@ -21,14 +21,45 @@ module Fluent
     class ProxysqlQueryLogInput < Fluent::Plugin::Input
       Fluent::Plugin.register_input("proxysql_query_log", self)
 
+      helpers :storage
+
+      DEFAULT_STORAGE_TYPE = 'local'
+
       config_param :path, :string
       config_param :read_from_head, :bool, default: false
 
+      config_section :storage do
+        config_set_default :usage, 'positions'
+        config_set_default :@type, DEFAULT_STORAGE_TYPE
+        config_set_default :persistent, false
+      end
+
+      def configure(conf)
+        super
+        @pos_storage = storage_create(usage: 'positions', type: DEFAULT_STORAGE_TYPE, conf: conf)
+      end
+
       def start
         super
+
+        Signal.trap('INT', 'TERM') do |signo|
+          @io.close
+          exit 0
+        end
+
         parser = ProxysqlQueryLog::Parser.new
 
-        parser.load_file(@path).each do |query|
+        @io = File.open(@path)
+        seek
+
+        while true
+          raw_total_bytes = @io.read(1)
+          next unless raw_total_bytes
+
+          total_bytes = raw_total_bytes.unpack('C')[0]
+          @io.seek(7, IO::SEEK_CUR)
+          raw = @io.read(total_bytes)
+          query = parser.parse(StringIO.new(raw, 'r+'))
           record = {
               'thread_id' => query.thread_id,
               'username' => query.username,
@@ -39,11 +70,19 @@ module Fluent
               'start_time' => Time.at(query.start_time/1000/1000),
               'end_time' => Time.at(query.end_time/1000/1000),
               'duration' => query.end_time - query.start_time,
-              'digest' => query.digest
+              'digest' => query.digest,
+              'query' => query.query
           }
           router.emit('', query.start_time, record)
+          @pos_storage.put(:journal, @io.pos)
         end
+      end
 
+      private
+
+      def seek
+        cursor = @pos_storage.get(:journal)
+        @io.seek(cursor, IO::SEEK_SET)
       end
     end
   end
