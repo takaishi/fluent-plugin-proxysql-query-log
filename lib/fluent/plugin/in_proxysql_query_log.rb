bda99a91f12bd0dc4320f543abcddf52eb94fec9
r_takaishi <ryo.takaishi.0@gmail.com>
2018-05-27 14:56:14 +0900

    split file watcher class

--- lib/fluent/plugin/in_proxysql_query_log.rb
+++ lib/fluent/plugin/in_proxysql_query_log.rb
@@ -14,9 +14,10 @@
 # limitations under the License.
 
 require 'cool.io'
-
-require "fluent/plugin/input"
 require 'proxysql_query_log/parser'
+
+require 'fluent/plugin/input'
+require 'fluent/plugin/in_proxysql_query_log/watcher'
 
 module Fluent
   module Plugin
@@ -93,62 +94,6 @@ module Fluent
             path
           end
         end.flatten
-      end
-    end
-
-    class Watcher < Coolio::StatWatcher
-      def initialize(path, interval, pos_storage, router, log)
-        super(path, interval)
-
-        @pos_storage = pos_storage
-        @router = router
-        @log = log
-        read
-      end
-
-      def seek(path)
-        cursor = @pos_storage.get(path)
-        if cursor
-          @io.seek(cursor, IO::SEEK_SET)
-        end
-      end
-
-      def on_change(previous, current)
-        @log.debug ("change: #{@path}")
-        @log.debug ("previous: #{previous}")
-        @log.debug ("current: #{current}")
-        read
-      end
-
-      def read
-        parser = ProxysqlQueryLog::Parser.new
-        @io = File.open(@path)
-        seek(@path)
-
-        while
-        raw_total_bytes = @io.read(1)
-          return unless raw_total_bytes
-
-          total_bytes = raw_total_bytes.unpack('C')[0]
-          @io.seek(7, IO::SEEK_CUR)
-          raw = @io.read(total_bytes)
-          query = parser.parse(StringIO.new(raw, 'r+'))
-          record = {
-              'thread_id' => query.thread_id,
-              'username' => query.username,
-              'schema_name' => query.schema_name,
-              'client' => query.client,
-              'HID' => query.hid,
-              'server' => query.server,
-              'start_time' => Time.at(query.start_time/1000/1000).utc.strftime('%Y-%m-%d %H:%M:%S'),
-              'end_time' => Time.at(query.end_time/1000/1000).utc.strftime('%Y-%m-%d %H:%M:%S'),
-              'duration' => query.end_time - query.start_time,
-              'digest' => query.digest,
-              'query' => query.query
-          }
-          @router.emit('', query.start_time/1000/1000, record)
-          @pos_storage.put(@path, @io.pos)
-        end
       end
     end
   end
