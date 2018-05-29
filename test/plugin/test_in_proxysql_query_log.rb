0f38353e832e8e490ac97182d1acfa97cdb295b6
r_takaishi <ryo.takaishi.0@gmail.com>
2018-05-29 14:45:31 +0900

    move to helper

--- test/plugin/test_in_proxysql_query_log.rb
+++ test/plugin/test_in_proxysql_query_log.rb
@@ -1,47 +1,6 @@
 require "helper"
 require "fluent/plugin/in_proxysql_query_log.rb"
 
-module ProxysqlQueryLog
-  class Query
-    def total_length
-      len = 0
-
-      # thread_id
-      len += 2
-
-      # username
-      len += (1 + username.size)
-
-      # schema_name
-      len += (1 + schema_name.size)
-
-      # client
-      len += (1 + client.size)
-
-      # hid
-      len += 1
-
-      # server
-      len += (1 + server.size)
-
-      # start_time
-      len += (1 + 8)
-
-      # end_time
-      len += (1 + 8)
-
-      # digest
-      len += (1 + digest.size)
-
-      # query
-      len += (1 + query.size)
-
-
-      len
-    end
-  end
-end
-
 class ProxysqlQueryLogInputTest < Test::Unit::TestCase
   setup do
     Fluent::Test.setup
@@ -94,40 +53,9 @@ class ProxysqlQueryLogInputTest < Test::Unit::TestCase
     q.end_time = 1525944256367837
     q.digest = '0xD69C6B36F32D2EAE'
     q.query = 'SELECT * FROM test'
-    buf = ''
-    io = StringIO.new(buf)
 
-    io.write([0].pack('C*'))
-    io.write([q.thread_id].pack('C*'))
-
-    io.write([q.username.size].pack('C*'))
-    io.write(q.username)
-
-    io.write([q.schema_name.size].pack('C*'))
-    io.write(q.schema_name)
-
-    io.write([q.client.size].pack('C*'))
-    io.write(q.client)
-
-    io.write([q.hid].pack('C*'))
-
-    io.write([q.server.size].pack('C*'))
-    io.write(q.server)
-
-    io.write([0xfe].pack('C*'))
-    io.write([q.start_time].pack('Q*'))
-
-    io.write([0xfe].pack('C*'))
-    io.write([q.end_time].pack('Q*'))
-
-    io.write([0xfe].pack('C*'))
-    io.write(q.digest.gsub(/0x/, '').scan(/.{1,8}/).map{|s| s.hex}.pack('I*'))
-
-    io.write([q.query.size].pack('C*'))
-    io.write(q.query)
-
-    f.write([q.total_length, 0, 0, 0, 0, 0, 0, 0].pack('C*'))
-    f.write(buf)
+    f.write([total_length(q), 0, 0, 0, 0, 0, 0, 0].pack('C*'))
+    f.write(to_binary(q))
   end
 
   def create_driver(conf)
